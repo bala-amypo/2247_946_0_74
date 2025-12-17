f755ce29ea461fcb4de0ecfbc3a1bb93acb1ea73
bala-amypo <bala@amypo.in>
2025-12-17 09:09:53 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -2,44 +2,50 @@ package com.example.demo.entity;
 
 import jakarta.persistence.*;
 
-public class Student{
-    @Id
-    private int id;
-    private String sname;
-    private String semail;
-    private float cgpa;
+@Entity
+// @Table(name = "student")
+public class Student {
 
-    public int getId(){
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    // Getters & Setters
+    public Long getId() {
         return id;
     }
-    public void setId(int id){
-        this.id=id;
-    }
-    public String getSname(){
-        return sname;
-    }
-    public void setSname(String sname){
-        this.sname=sname;
-    }
-    public String getSemail(){
-        return semail;
-    }
-    public void setSemail(String semail){
-        this.semail=semail;
-    }
-    public float getCgpa(){
-        return cgpa;
-    }
-    public void setCgpa(float cgpa){
-        this.cgpa=cgpa;
-    }
-    public Student(int id, String sname, String semail, float cgpa) {
+
+    public void setId(Long id) {
         this.id = id;
-        this.sname = sname;
-        this.semail = semail;
-        this.cgpa = cgpa;
     }
-     public Student() {
+
+    public String getName() {
+        return name;
     }
-    
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email=email;
+      }
+
+
+    // Constructors
+    public Student() {
+    }
+
+    public Student(Long id,String name,String email,LocalDate dob,float cgpa) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
 }
