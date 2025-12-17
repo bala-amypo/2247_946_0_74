a3b67c70d42dcb125e15e1c0119844ef69dafeec
bala-amypo <bala@amypo.in>
2025-12-17 06:36:16 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import java.util.List;
 import com.example.demo.entity.Student;
 
-public interface StudentService {
+public interface StudentService { //abstract method can be only in interface
 
 //abstract
     Student createData(Student stu);
